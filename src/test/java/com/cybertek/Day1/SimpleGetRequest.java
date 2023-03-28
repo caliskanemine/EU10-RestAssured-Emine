dd72fb0f3822506d286791da6b23ec98f37134b7
Emine Caliskan <caliskanemine95@gmail.com>
2023-03-29 01:43:47 +0300

    Created the project and Day1 is added as class

---
+++ src/test/java/com/cybertek/Day1/SimpleGetRequest.java
@@ -0,0 +1,25 @@
+package com.cybertek.Day1;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+
+    String url= "http://44.197.123.56:8000/api/spartans";
+
+    @Test
+    public void test1(){
+
+        Response response = RestAssured.get(url); //alt + enter to get response
+
+        //print response status code
+        System.out.println(response.statusCode());
+
+        //print response body
+        response.prettyPrint();
+
+    }
+
+
+}
